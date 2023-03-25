d4f9c0d4483a297d1fb928d0a9dfeb19a7f30e5c
braxtonshaffie <braxtonshaffie@gmail.com>
2023-03-25 17:33:06 +0300

    Done with the challenge

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const newTaskForm = document.getElementById("create-task-form");
+
+  newTaskForm.addEventListener("submit", (e) => {
+    // prevent default form submission behavior
+    e.preventDefault();
+
+    // get the input value from the form
+    const newTaskDescription = document.getElementById("new-task-description");
+    const taskDescription = newTaskDescription.value;
+
+    // create a new task element and add it to the task list
+    const newTask = document.createElement("li");
+    newTask.textContent = taskDescription;
+    const taskList = document.getElementById("tasks");
+    taskList.appendChild(newTask);
+
+    // reset form
+    e.target.reset();
+  });
 });
